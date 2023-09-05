733b582b929e81de7f0b98e0fbf4da047dc13000
Artyom Sosedka <sostema@gmail.com>
2023-09-05 14:27:10 +0300

    feat: add go.nvim

--- plugins.lua
+++ plugins.lua
@@ -73,6 +73,20 @@ local plugins = {
       { "<leader>cp", "<cmd>Copilot<CR>", desc = "Copilot" },
     },
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
