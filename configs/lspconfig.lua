35e28986b2cae12eabb0199894d46f18d53878e3
Artyom Sosedka <sostema@gmail.com>
2023-09-11 11:06:57 +0300

    feat: add bashls

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pyright", "ruff_lsp" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pyright", "ruff_lsp", "bashls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -14,5 +14,5 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
+--
 -- lspconfig.pyright.setup { blabla}
