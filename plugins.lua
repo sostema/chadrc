6367c5fcbdae1efc2310dc655958d51cea334219
Artyom Sosedka <sostema@gmail.com>
2023-09-11 11:18:40 +0300

    style: refactor go.nvim section in plugins

--- plugins.lua
+++ plugins.lua
@@ -89,17 +89,17 @@ local plugins = {
   },
   {
     "ray-x/go.nvim",
-    dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
   {
     "Dhanus3133/LeetBuddy.nvim",
